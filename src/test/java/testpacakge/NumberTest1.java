65f081db117c9e66eed4fff22703a4187856de23
nksugumaran16 <snaveen0507@gmail.com>
2021-08-12 14:04:13 -0500

    Added NumberTest1.java

---
+++ src/test/java/testpacakge/NumberTest1.java
@@ -0,0 +1,15 @@
+package testpacakge;
+
+public class NumberTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		for(i=0; i<=11;i++) {
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
